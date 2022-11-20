ab49b13af1ee6ff8fe99ebbd81877aafe089da32
vzze <lolo32778@gmail.com>
2022-11-20 12:40:56 +0200

    bg script

--- extension/background.js
+++ extension/background.js
@@ -1,12 +1,13 @@
 const storage = (browser.storage.session) ? browser.storage.session : browser.storage.sync;
 
 browser.tabs.onRemoved.addListener(async tabId => {
-    let data = await browser.storage.sync.get("volumes").catch(() => {});
+    const data = await browser.storage.sync.get("volumes").catch(() => {});
 
     if(!data) return;
     if(!data.volumes) return;
-    if(!data.volumes[tabId]) return;
+    if(!data.volumes[`${tabId}`]) return;
 
-    delete data.volumes[tabId];
-    storage.set({ volumes: data.volumes }).catch(() => {});
+    delete data.volumes[`${tabId}`];
+
+    await storage.set({ volumes: data.volumes }).catch(() => {});
 })
