1a0d7f6436f012e244e86927697d037284d43ae8
vzze <lolo32778@gmail.com>
2022-12-04 00:55:47 +0200

    update tabs callback

--- extension/background.js
+++ extension/background.js
@@ -19,6 +19,13 @@ browser.tabs.onUpdated.addListener(async tabId => {
     if(!data.volumes) return;
     if(data.volumes[tabId] == undefined) return;
 
+    const res = await browser.tabs.executeScript(tabId, {
+        code: `document.querySelectorAll("video, audio")[0].volume`
+    }).catch(() => {});
+
+    if(Math.floor(res[0] * 100) != data.volumes[tabId])
+        data.volumes[tabId] = Math.floor(res[0] * 100)
+
     await browser.tabs.executeScript(tabId, {
         code: `document.querySelectorAll("video, audio").forEach(elem => elem.volume = ${data.volumes[tabId] / 100})`
     }).catch(() => { data.volumes[tabId] = 100; });
