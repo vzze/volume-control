a300eddc3c24760f1fd5e250a1e36448c15c4854
vzze <lolo32778@gmail.com>
2024-07-04 13:46:02 +0300

    add non-linear scaling option

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,5 @@
-const storage = (browser.storage.session) ? browser.storage.session : browser.storage.sync;
+const storage     = browser.storage.session;
+const syncStorage = browser.storage.sync;
 
 browser.tabs.onRemoved.addListener(async tabId => {
     const data = await storage.get("volumes").catch(() => {});
@@ -14,10 +15,20 @@ browser.tabs.onRemoved.addListener(async tabId => {
 
 browser.tabs.onUpdated.addListener(async tabId => {
     const data = await storage.get("volumes").catch(() => {});
+    const sync = await syncStorage.get("nonLinearVolume").catch(() => {});
 
     if(!data) return;
     if(!data.volumes) return;
     if(data.volumes[tabId] == undefined) return;
+
+    const nonLinearVolume = (sync.nonLinearVolume) ? true : false;
+
+    const newVolume = (vol) => {
+        if(nonLinearVolume)
+            return (vol / 100) ** 2;
+        else
+            return (vol / 100);
+    }
 
     const res = await browser.tabs.executeScript(tabId, {
         code: `document.querySelectorAll("video, audio")[0].volume`
@@ -27,7 +38,7 @@ browser.tabs.onUpdated.addListener(async tabId => {
         data.volumes[tabId] = Math.floor(res[0] * 100)
 
     await browser.tabs.executeScript(tabId, {
-        code: `document.querySelectorAll("video, audio").forEach(elem => elem.volume = ${data.volumes[tabId] / 100})`
+        code: `document.querySelectorAll("video, audio").forEach(elem => elem.volume = ${newVolume(data.volumes[tabId])})`
     }).catch(() => { data.volumes[tabId] = 100; });
 
     await storage.set({ volumes: data.volumes }).catch(() => {});
